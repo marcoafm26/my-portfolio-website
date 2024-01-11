be6a3efc270821b041b979d64d56221c0057a8af
Marco Aurélio <marcoaureliofm26@gmail.com>
2024-01-11 14:44:47 -0300

    concluded scrollup component

--- src/components/scrollup/ScrollUp.jsx
+++ src/components/scrollup/ScrollUp.jsx
@@ -1,6 +1,16 @@
 // import React from 'react';
 import './scrollup.css';
 const ScrollUp = () => {
+    const verifyActualPositionY = () => {
+        const scrollUp = document.querySelector('.scrollup');
+        if (scrollY >= 400) {
+            scrollUp.classList.add('show-scroll');
+        } else {
+            scrollUp.classList.remove('show-scroll');
+        }
+    };
+
+    document.addEventListener('scroll', () => verifyActualPositionY());
     return (
         <a href="#home" className="scrollup">
             <i className="uil uil-arrow-up scrollup__icon"></i>
