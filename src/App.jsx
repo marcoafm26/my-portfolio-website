e3b227f03238429d5f78cd77ff029255165dfc9e
Marco Aurélio <marcoaureliofm26@gmail.com>
2024-01-11 13:18:26 -0300

    concluded skills component with some fixes on divs organization

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import './App.css';
 import About from './components/about/About';
 import Header from './components/header/header';
 import Home from './components/home/Home';
+import Skills from './components/skills/Skills';
 function App() {
     return (
         <div>
             <Header />
             <main className="main">
                 <Home />
-                <About/>
+                <About />
+                <Skills />
             </main>
         </div>
     );
